c386c1c49432663e6afe8d614a9b71380189863c
Colby Birkhead <birkheadc@gmail.com>
2023-01-23 16:48:45 +0900

    build reactive user registration form; refactor some styling

---
+++ src/app/register/register-form/register-form-input/register-form-input.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { RegisterFormComponent } from '../register-form.component';
+
+@Component({
+  selector: 'app-register-form-input',
+  templateUrl: './register-form-input.component.html',
+  styleUrls: ['./register-form-input.component.css']
+})
+export class RegisterFormInputComponent {
+  @Input()
+  control!: FormControl
+  @Input()
+  id: string = "";
+  @Input()
+  type: string = "";
+}
--- src/app/register/register-form/register-form.component.ts
+++ src/app/register/register-form/register-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, Injectable } from '@angular/core';
+import { Component, Injectable, OnInit } from '@angular/core';
+import { AbstractControl, Form, FormControl, FormGroup, NgForm, ValidatorFn, Validators } from '@angular/forms';
 import { UserService } from 'src/app/user/user.service';
 
 @Component({
@@ -6,15 +7,46 @@ import { UserService } from 'src/app/user/user.service';
   templateUrl: './register-form.component.html',
   styleUrls: ['./register-form.component.css']
 })
-export class RegisterFormComponent {
+export class RegisterFormComponent implements OnInit {
 
-  private userService: UserService
+  userService: UserService;
+  userForm!: FormGroup;
 
   constructor(userService: UserService) {
     this.userService = userService;
   }
 
-  createUser = (event: FormEvent) => {
-    
+  ngOnInit(): void {
+    this.userForm = this.generateUserForm();
+    this.userForm.get('confirmPassword')?.addValidators([
+      this.controlValuesMatch(
+        this.userForm.get('password') as FormControl,
+        this.userForm.get('confirmPassword') as FormControl
+      )
+    ]);
+  }
+
+  generateUserForm(): FormGroup {
+    return new FormGroup({
+      username: new FormControl(null, [Validators.required, Validators.maxLength(32), Validators.pattern("^[a-zA-Z0-9 ]*$")]),
+      emailAddress: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(8), Validators.maxLength(64)]),
+      confirmPassword: new FormControl(null, [Validators.required]),
+      sendMeMail: new FormControl(false),
+      agreeToTos: new FormControl(false, Validators.requiredTrue)
+    });
+  }
+
+  controlValuesMatch(a: FormControl, b: FormControl): ValidatorFn {
+    return () => a.value === b.value ? null : { valuesMatch: false };
+  }
+
+  getFormControl(controlName: string): FormControl {
+    return this.userForm.controls[controlName] as FormControl;
+  }
+
+  onSubmit() {
+    console.log("create user...", this.userForm);
+    console.log(this.userForm.valid ? "This form is valid." : "THIS FORM IS NOT VALID!");
   }
 }
